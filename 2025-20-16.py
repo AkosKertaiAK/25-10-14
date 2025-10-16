bd69473be21b25db27036e65df2f51ed7f6d18f4
AkosKertaiAK <kertaia.24d@acsjszki.hu>
2025-10-16 12:08:56 +0200

    Create 2025-20-16.py

---
+++ 2025-20-16.py
@@ -0,0 +1,10 @@
+import math
+
+x1=float(input("Első x:"))
+x2=float(input("Második x:"))
+y1=float(input("Első y:"))
+y2=float(input("Második y:"))
+
+tavolsag=math.sqrt((x2-x1)**2+(y2-y1)**2)
+
+print(tavolsag)
